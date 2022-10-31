36a7a89b667f0221568a956ee95282f5fa87f629
Mikhail S <90897899+SurfWinter@users.noreply.github.com>
2022-10-31 16:17:55 +0300

    Add files via upload

--- Js/Operations.js
+++ Js/Operations.js
@@ -1,3 +1,38 @@
+(function() {
+	window.AudioContext = window.AudioContext || window.webkitAudioContext;
+	if (window.AudioContext) {
+		window.audioContext = new window.AudioContext();
+	}
+	let fixAudioContext = function (e) {
+		if (window.audioContext) {
+			// Create empty buffer
+			let buffer = window.audioContext.createBuffer(1, 1, 22050);
+			let source = window.audioContext.createBufferSource();
+			source.buffer = buffer;
+			// Connect to output (speakers)
+			source.connect(window.audioContext.destination);
+			// Play sound
+			if (source.start) {
+				source.start(0);
+			} else if (source.play) {
+				source.play(0);
+			} else if (source.noteOn) {
+				source.noteOn(0);
+			}
+		}
+		// Remove events
+		document.removeEventListener('touchstart', fixAudioContext);
+		document.removeEventListener('touchend', fixAudioContext);
+	};
+	// iOS 6-8
+	document.addEventListener('touchstart', fixAudioContext);
+	// iOS 9
+	document.addEventListener('touchend', fixAudioContext);
+})();
+
+/* ----------------------аудиофикс------------------------------*/
+
+
 const motherConatiner = document.querySelector('.motherContainer')
 const hpBlock = document.querySelector('.hpBlock') 
 const hpNumber = document.querySelector('.hpNumber') 
